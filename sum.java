4fe0def9de23658982760e33a859d3568598dab6
Rohit Patil <rohitpatil0086@gmail.com>
2022-07-29 15:47:01 +0530

    modifying some inside sum.java

--- sum.java
+++ sum.java
@@ -1,8 +1,8 @@
-public class Sum {
+public class sum {
  
     public static void main(String[] args){
         int a=9;
         int b=6;
-        System.out.println("sum of a and b is : " + (a+b));
+        System.out.println("sum a and b is : " + (a+b));
     }
 }
